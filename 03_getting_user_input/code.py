8de36c19d92357a8e53eba6d564673405013822d
Ay-Aal <88855746+Cynosure11@users.noreply.github.com>
2023-05-28 14:16:52 -0400

    Python Refresher
    Python refresher. Leaning python basics one more time

---
+++ 03_getting_user_input/code.py
@@ -0,0 +1,7 @@
+name = input("What is your name?")
+print(f"Hello, {name}")
+
+size_input = input("How big is your house (in square feet): ")
+square_feet = int(size_input)
+square_metres = square_feet / 10.8
+print(f"{square_feet} Square feet is {square_metres:.2f} Sqaure metres")
