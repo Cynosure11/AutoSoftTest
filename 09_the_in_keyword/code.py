a54a06dca6dedca7dde25233b31a405ef4d1ce00
Aial Atlasov <atlasov.aial@gmail.com>
2023-05-28 16:39:45 -0400

    lesson #10 is done

---
+++ 09_the_in_keyword/code.py
@@ -0,0 +1,6 @@
+movies_watched = {"The Matrix", "Green Book", "Her"}
+lower = movies_watched
+
+user_movie = input("Enter something you've watched recently: ")
+
+print( user_movie in movies_watched)
